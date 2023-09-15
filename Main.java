f7494061bc1ef79a31cb5c9a8a7a83b4d7a20e79
Clara Monteiro Menezes <claramonteiro@mobitbrasil.com.br>
2023-09-15 08:57:17 -0300

    mais alteracoes

--- Main.java
+++ Main.java
@@ -9,11 +9,14 @@ public class Main {
         Motor motorCarroEsportivo = new Motor("v12", 700);
         CarroEsportivo meuCarroEsportivo = new CarroEsportivo("Ferrari", "488 GTB", 2023, motorCarroEsportivo, 320);
         meuCarroEsportivo.setVelocidadeMaxima(300);
+        System.out.println("Carro esportivo: ");
         meuCarroEsportivo.acelerar();
         System.out.println(meuCarroEsportivo);
         meuCarroEsportivo.setVelocidadeMaxima(80);
         System.out.println(meuCarroEsportivo);
+        System.out.println("\n----------\n");
 
+        System.out.println("Carro: ");
         Motor motorCarro = new Motor("v8", 500);
         Carro meuCarroComMotor = new Carro("Chevrolet", "Camaro", 2023, motorCarro);
         meuCarroComMotor.rodar(100);
